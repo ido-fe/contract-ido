52561a06f166d003022de018a324e7f9e4b8a4f8
max <cao.4321519@163.com>
2024-10-03 22:46:32 +0800

    add burn

--- ERC20TokenWrapped.sol
+++ ERC20TokenWrapped.sol
@@ -4,9 +4,10 @@ pragma solidity 0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20TokenWrapped is ERC20Permit, ERC20Capped, Ownable {
+contract ERC20TokenWrapped is ERC20Permit, ERC20Capped, Ownable, ERC20Burnable {
     // Decimals
     uint8 private immutable _decimals;
 
